924945e517018fe067e54b18502034cd20cbff72
Matej Ramuta <matej.ramuta@gmail.com>
2016-04-20 22:05:02 +0200

    some optimization of readability

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import BeautifulSoup
-import urllib2
+from BeautifulSoup import BeautifulSoup
+from urllib2 import urlopen
 from model import Person
 from util import create_csv_file
 
 url = 'https://scrapebook22.appspot.com/'
 
-html = urllib2.urlopen(url).read()
+response = urlopen(url).read()
 
-soup = BeautifulSoup.BeautifulSoup(html)
+soup = BeautifulSoup(response)
 
 print soup.html.head.title.string  # should be: Scrapebook | by SmartNinja
 
@@ -16,8 +16,8 @@ persons = []
 for link in soup.findAll("a"):
     if link.string == "See full profile":
         person_url = "https://scrapebook22.appspot.com" + link["href"]
-        person_html = urllib2.urlopen(person_url).read()
-        person_soup = BeautifulSoup.BeautifulSoup(person_html)
+        person_html = urlopen(person_url).read()
+        person_soup = BeautifulSoup(person_html)
 
         email = person_soup.find("span", attrs={"class": "email"}).string
         name = person_soup.find("div", attrs={"class": "col-md-8"}).h1.string
